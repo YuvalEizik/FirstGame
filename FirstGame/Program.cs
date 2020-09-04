39335f70b7e493a76f0106378afc05d06f397632
Yuval Eizik <yuval.eizik3669@gmail.com>
2020-09-04 15:41:37 +0300

    Add project files.

---
+++ FirstGame/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace FirstGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title="Game Server";
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ FirstGame/Server.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace FirstGame
+{
+    class Server
+    {       
+
+        public static int MaxPlayers { get; private set; }
+        public static int Port { get; private set; }
+
+        private static TcpListener tcpListener;
+
+        public void Start(int maxPlayers, int _port)
+        {
+            MaxPlayers = maxPlayers;
+            Port = _port;
+
+            tcpListener = new TcpListener(IPAddress.Any, Port);
+            tcpListener.Start();
+            tcpListener.BeginAcceptTcpClient(new AsyncCallback(TCPConnectCallback,null);
+
+            Console.WriteLine($"Server started on {Port},");
+        }
+    }
+}
